9b8c808d96ba89e8fa56929cff265170da0a6055
Lucas Barbosa da Silva <145168605+Lucasbarbosa332@users.noreply.github.com>
2024-05-16 00:52:45 -0300

    Add files via upload

---
+++ lucas/src/js.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".menu-button").click(function(){
+    $(".menu-bar").toggleClass( "open" );
+    })
+})
+
+function updatemenu() {
+    if (document.getElementById('responsive-menu').checked == true) {
+      document.getElementById('menu').style.borderBottomRightRadius = '0';
+      document.getElementById('menu').style.borderBottomLeftRadius = '0';
+    }else{
+      document.getElementById('menu').style.borderRadius = '10px';
+    }
+  }
+  
